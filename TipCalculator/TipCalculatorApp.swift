bae6d5caafa90da4aa99d43ba8f6dbf8c0d4bd19
hanakastrati <hanakast@outlook.com>
2025-03-03 08:18:08 -0500

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Hana Kastrati on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
