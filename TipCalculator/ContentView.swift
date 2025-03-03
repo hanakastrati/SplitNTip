71ac2b0594a66c1b4ef6425c2c3fddff687bd16a
hanakastrati <hanakast@outlook.com>
2025-03-03 09:46:59 -0500

    Basic Tip Calculator

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -8,12 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    //understand @state & private var
+    @State private var billAmount = ""
+    @State private var tipPercent = 15
+    private let tipOptions = [10, 15, 18, 20, 25]
+    
+    var tipAmount: Double {
+        //convert string to double
+        let bill = Double(billAmount) ?? 0
+        
+        //calculate tip
+        let tipValue = bill * (Double(tipPercent) / 100)
+        
+        //return values
+        return tipValue
+    }
+    
+    var totalAmount: Double {
+        //convert string to double
+        let bill = Double(billAmount) ?? 0
+        //add tip to total for total amount
+        let total = bill + tipAmount
+        //return total amount
+        return total
+
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Tip Calculator")
+                .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.center)
+            
+            TextField("Enter bill amount", text: $billAmount)
+                .keyboardType(.decimalPad)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Picker("Tip Percentage", selection: $tipPercent){
+                ForEach(tipOptions, id: \.self){ tipPercent in
+                    Text("\(tipPercent)%")
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
+            Text("Tip amount: $\(tipAmount, specifier: "%.2f")")
+                .font(.body)
+            
+            Text("Total amount: $\(totalAmount, specifier: "%.2f")")
+                .font(.body)
+                .padding()
         }
         .padding()
     }
