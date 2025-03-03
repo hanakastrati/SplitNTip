7270d86a4ad1172a5297c6db1838fc21a80a0183
hanakastrati <hanakast@outlook.com>
2025-03-03 11:43:37 -0500

    Aesthetic changes

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -32,34 +32,42 @@ struct ContentView: View {
         return total
 
     }
-    
     var body: some View {
-        VStack {
-            Text("Tip Calculator")
+        VStack(){
+            Text("Tip Calculator 💰")
                 .font(.largeTitle)
                 .bold()
                 .multilineTextAlignment(.center)
+                .padding(.top, 20)
+            Spacer()
             
-            TextField("Enter bill amount", text: $billAmount)
-                .keyboardType(.decimalPad)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            Picker("Tip Percentage", selection: $tipPercent){
-                ForEach(tipOptions, id: \.self){ tipPercent in
-                    Text("\(tipPercent)%")
+            VStack() {
+                TextField("Enter bill amount", text: $billAmount)
+                    .keyboardType(.decimalPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Text("How much do you want to tip?")
+                Picker("Tip Percentage", selection: $tipPercent){
+                    ForEach(tipOptions, id: \.self){ tipPercent in
+                        Text("\(tipPercent)%")
+                    }
                 }
+                .pickerStyle(MenuPickerStyle())
+                
+                Spacer().frame(height: 15)
+                
+                Text("Your tip is: $\(tipAmount, specifier: "%.2f")")
+                    .font(.body)
+                
+                Text("Your total is: $\(totalAmount, specifier: "%.2f")")
+                    .font(.body)
             }
-            .pickerStyle(SegmentedPickerStyle())
-            
-            Text("Tip amount: $\(tipAmount, specifier: "%.2f")")
-                .font(.body)
-            
-            Text("Total amount: $\(totalAmount, specifier: "%.2f")")
-                .font(.body)
-                .padding()
+            .padding()
+            Spacer()
         }
-        .padding()
+        
+        
     }
 }
 
